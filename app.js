a11a105c3ac57ab5c19634e2f0b3893a7030ba07
k-isaka <isk.rugby@gmail.com>
2022-11-06 12:30:50 +0900

    [feat] generate json object

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 /* 1. expressモジュールをロードし、インスタンス化してappに代入。*/
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const axios = require("axios").default;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
+});
+
+app.post('/registration', (req, res)=>{
+    console.log(req.body);
+    console.log(req.body.last_name);
+    res.send(req.body);
 });
 
 /* 2. listen()メソッドを実行して8080番ポートで待ち受け。*/
