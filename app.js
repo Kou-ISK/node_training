bd5cf98c1da571d669a3a824116e32d2512642d7
k-isaka <isk.rugby@gmail.com>
2022-11-08 23:25:54 +0900

    [feat] request to LINE_NOTIFY (failed)

--- app.js
+++ app.js
@@ -2,7 +2,13 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const axios = require("axios").default;
+const JSON = require('JSON');
+
+var LINE_NOTIFY_TOKEN = "文字列";
+var LINE_NOTIFY_API = "https://notify-api.line.me/api/notify";
+
+
+const request = require('request');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -11,13 +17,30 @@ app.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
-app.post('/registration', (req, res)=>{
-    console.log(req.body);
+
+app.post('/registration', (req, res) => {
     console.log(req.body.last_name);
-    res.send(req.body);
+    var msg = toString(req.body.last_name);
+    //LINEにメッセージを送る
+    //400エラーばかり返されている
+    request.post(
+        {
+            "url": LINE_NOTIFY_API,
+            "content-type": "application/x-www-form-urlencoded",
+            "method": "post",
+            "headers": {
+                "Authorization": "Bearer " + LINE_NOTIFY_TOKEN
+            },
+            "payload": { "message": msg }
+        }
+        , function (err, body, data) {
+            console.log(data);
+        }
+    );
+    // res.send(req.body);
 });
 
-/* 2. listen()メソッドを実行して8080番ポートで待ち受け。*/
-app.listen(8080);
+/* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
+app.listen(3000);
 
 
