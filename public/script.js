3f63fee56cd6ab4373a551a273835730565f22ed
k-isaka <isk.rugby@gmail.com>
2023-01-01 19:50:25 +0900

    feat:Modalを新規実装(WIP)

---
+++ public/script.js
@@ -0,0 +1,29 @@
+$(function () {
+    $('.user-registration-show').click(function () {
+        $('#user-registration-modal').fadeIn();
+    });
+
+    $('#user-registration-close-modal').click(function () {
+        $('#user-registration-modal').fadeOut();
+    });
+});
+
+$(function () {
+    $('.find-all-user-show').click(function () {
+        $('#find-all-user-modal').fadeIn();
+    });
+
+    $('#find-all-user-close-modal').click(function () {
+        $('#find-all-user-modal').fadeOut();
+    });
+});
+
+$(function () {
+    $('.find-user-by-id-show').click(function () {
+        $('#find-user-by-id-modal').fadeIn();
+    });
+
+    $('#find-user-by-id-close-modal').click(function () {
+        $('#find-user-by-id-modal').fadeOut();
+    });
+});
